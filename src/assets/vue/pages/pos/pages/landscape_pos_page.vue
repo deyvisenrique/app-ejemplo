6a75dd4ee37775e891468d00f0f9cbccb2e8c434
rash07 <sh.raul.jgb@gmail.com>
2022-08-20 14:04:45 -0500

    feature | borde en buscador de modo pos

--- src/assets/vue/pages/pos/pages/landscape_pos_page.vue
+++ src/assets/vue/pages/pos/pages/landscape_pos_page.vue
@@ -9,10 +9,10 @@
                     <f7-card class="card-100 padding no-shadow" color="red" style="min-height: 90%">
 
                         <f7-row>
-                            <f7-col width="70">
-                                <div class="searchbar searchbar-inline pt-1" style="margin-left:-5px">
+                            <f7-col width="70" style="border: 1px solid #8e8e93;border-radius: 5px;">
+                                <div class="searchbar searchbar-inline" style="margin-left:-5px; margin: 5px">
                                     <div class="searchbar-input-wrap">
-                                        <input type="search" placeholder="Buscar" style="font-size:12px;" v-model="form_search.input" @input="searchRecords"/>
+                                        <input type="search" placeholder="Buscar" v-model="form_search.input" @input="searchRecords"/>
                                         <i class="searchbar-icon"></i>
                                         <button  class="input-clear-button" @click="clickClearInput"></button>
                                     </div>
