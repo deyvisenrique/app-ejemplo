c2f99f7463c23b4eeaee2541a5684555db8393cd
Your Name <you@example.com>
2022-10-30 18:04:12 -0500

    fixed | actualizar webpack para generar .aab

--- config/webpack.release.js
+++ config/webpack.release.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 
 const CordovaHtmlOutputPlugin = require('../webpack/plugins/CordovaHtmlOutputPlugin.js');
 const TerserPlugin = require('terser-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
+// const CleanPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = function() {
   return webpackMerge(commonConfig({ mode: 'production' }), {
@@ -20,12 +22,18 @@ module.exports = function() {
       new TerserPlugin({
         extractComments: false
       }),
-      new CleanPlugin('www', {
-        root: path.join(__dirname, '.'),
+      new CleanWebpackPlugin({
         dry: false,
         verbose: false,
-        exclude: ['index.html']
+        cleanOnceBeforeBuildPatterns: ['!index.html'],
+        cleanAfterEveryBuildPatterns: ['!index.html']
       })
+      // new CleanPlugin('www', {
+      //   root: path.join(__dirname, '.'),
+      //   dry: false,
+      //   verbose: false,
+      //   exclude: ['index.html']
+      // })
     ]
   });
 };
